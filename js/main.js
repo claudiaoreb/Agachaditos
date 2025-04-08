351298e8e6440f1df492445bc7abddec14ac4e56
claudiaoreb <2020151521tls@gmail.com>
2025-04-08 11:40:27 -0500

    Primer commit de Agachaditos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleLogin() {
+  alert('Aquí irá el formulario de login / registro.');
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Mensaje enviado (simulado). Integra EmailJS aquí.');
+});
